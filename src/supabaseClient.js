46bb1cc79ee50a2318735c4dc04ee9740636ba0b
rishikaahuja555 <rishikaahuja303@gmail.com>
2026-05-17 17:11:00 +0530

    Updated supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = https://utjilgbpuyhjbrytofgs.supabase.co;
+const supabaseAnonKey = sb_publishable_ClGC4xGTFYB4jqU6bMiS8A_uSASQ6eL;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
